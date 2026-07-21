f54621d16e77a81286dd72e33e4c0375eea96dc8
Thomas <thomas.mellina@reseau.sncf.fr>
2026-07-21 15:11:25 +0200

    Update preload.js

--- assets/js/preload.js
+++ assets/js/preload.js
@@ -18,38 +18,46 @@ class Preloader {
 
     }
 
-    loadImage() {
+   loadImage() {
 
-        return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-            this.image = new Image();
+        this.image = new Image();
 
-            this.image.onload = () => resolve();
+        this.image.onload = () => {
+            console.log("✅ Image chargée");
+            resolve();
+        };
 
-            this.image.onerror = () => reject("Impossible de charger l'image.");
+        this.image.onerror = () => {
+            console.error("❌ Impossible de charger :", CONFIG.assets.image);
+            reject(`Impossible de charger ${CONFIG.assets.image}`);
+        };
 
-            this.image.src = CONFIG.assets.image;
+        this.image.src = CONFIG.assets.image;
 
-        });
+    });
 
-    }
-
+}
     loadAudio() {
 
-        return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-            this.audio = new Audio();
+        this.audio = new Audio();
 
-            this.audio.preload = "auto";
+        this.audio.preload = "auto";
 
-            this.audio.oncanplaythrough = () => resolve();
-
-            this.audio.onerror = () => reject("Impossible de charger le son.");
-
-            this.audio.src = CONFIG.assets.audio;
-
+        this.audio.addEventListener("loadedmetadata", () => {
+            resolve();
         });
 
-    }
+        this.audio.addEventListener("error", () => {
+            reject("Impossible de charger le son.");
+        });
+
+        this.audio.src = CONFIG.assets.audio;
+        this.audio.load();
+
+    });
 
 }
