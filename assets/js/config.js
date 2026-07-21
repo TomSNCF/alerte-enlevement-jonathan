be42fd3cc98d15dcb74ad3d5aa6b574dec8a8f32
Thomas <thomas.mellina@reseau.sncf.fr>
2026-07-21 15:35:26 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -8,38 +8,28 @@ Configuration générale
 const CONFIG = {
 
     appName: "Alerte Player",
-
-    version: "1.0.0",
+    version: "1.1.0",
 
     files: {
-
         image: "affiche.png",
-
         audio: "musique.wav"
-
     },
 
     texts: {
-
         loading: "Chargement de l'alerte...",
-
         title: "MESSAGE IMPORTANT",
-
         subtitle: "Touchez l'écran"
-
     },
 
     appearance: {
+        background: "#000",
+        initialZoom: 1.10,
+        finalZoom: 1.00
+    },
 
-        background: "#000000",
-
-        initialZoom: 1.00
-
-    }
+    fullscreen: true,
+    wakeLock: true
 
 };
 
 Object.freeze(CONFIG);
-Object.freeze(CONFIG.files);
-Object.freeze(CONFIG.texts);
-Object.freeze(CONFIG.appearance);
